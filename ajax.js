d36ffddfc364f704f0c004e0a961e6e01eaa511f
ttpatel <54196748+ttpatel@users.noreply.github.com>
2021-03-14 16:51:42 +0530

    completed

--- ajax.js
+++ ajax.js
@@ -2,31 +2,36 @@
 // for(var i=0;i<=100;i++)
 // { 
 //  document.getElementById('button').addEventListener('click', loadUsers);
- 
+
 // }
- function loadUsers() {
-     var xhr = new XMLHttpRequest();
-     xhr.open('GEt', 'https://jsonplaceholder.typicode.com/posts', true);
+function loadUsers() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GEt', 'https://jsonplaceholder.typicode.com/posts', true);
 
-     xhr.onload = function () {
+  xhr.onload = function () {
 
-         if (this.status == 200)
-             var users = JSON.parse(this.responseText);
+    if (this.status == 200)
+      var users = JSON.parse(this.responseText);
 
-         var output = '';
-         for (var i in users) {
+    var output = '';
+    for (var i in users) {
 
-             output +=`
+      output += `
              <div class="accordion-item">
              <h2 class="accordion-header" id="heading${i}">
                
-               <button class="accordion-button collapsed"  type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-               <div> 
-               ${users[i].userId} &nbsp; ${i}
+               <button class="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+               <div id ="usersid"> 
+                  BLOGID-${users[i].userId} &nbsp;USERID-${i}
                 <br>
-
-                ${users[i].title}
-                <span style="float:right">bc</span>  </div           
+                </div>
+               <div id="tit">
+                  <span id="titles"><strong id="big">TITLE-</strong>${users[i].title}</span>
+                     </div>          
+                        <div id="left">
+                          
+                        </div>
+                
                </button>
              </h2>
              <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="heading${i}" data-bs-parent="#accordionExample">
@@ -36,13 +41,14 @@
              </div>
            </div>
            <br>
+          <hr>
             `
-         }
+    }
 
 
 
-         document.getElementById('accordionExample').innerHTML = output;
-     }
-     xhr.send();
- }
- loadUsers();
+    document.getElementById('accordionExample').innerHTML = output;
+  }
+  xhr.send();
+}
+loadUsers();
